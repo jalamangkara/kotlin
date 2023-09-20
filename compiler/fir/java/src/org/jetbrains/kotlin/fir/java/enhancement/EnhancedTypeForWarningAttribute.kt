81b726779d067a5fffd1b66d90a92df3da04bc97
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2023-09-20 15:31:42 +0200

    [FIR] Save warning-level enhanced types to an attribute
    #KT-56989

---
+++ compiler/fir/java/src/org/jetbrains/kotlin/fir/java/enhancement/EnhancedTypeForWarningAttribute.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2010-2023 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.fir.java.enhancement
+
+import org.jetbrains.kotlin.fir.types.ConeAttributeWithConeType
+import org.jetbrains.kotlin.fir.types.ConeAttributes
+import org.jetbrains.kotlin.fir.types.ConeKotlinType
+import org.jetbrains.kotlin.fir.types.renderForDebugging
+import kotlin.reflect.KClass
+
+class EnhancedTypeForWarningAttribute(
+    override val coneType: ConeKotlinType,
+) : ConeAttributeWithConeType<EnhancedTypeForWarningAttribute>() {
+    override fun union(other: EnhancedTypeForWarningAttribute?): EnhancedTypeForWarningAttribute = other ?: this
+    override fun intersect(other: EnhancedTypeForWarningAttribute?): EnhancedTypeForWarningAttribute = other ?: this
+    override fun add(other: EnhancedTypeForWarningAttribute?): EnhancedTypeForWarningAttribute = other ?: this
+    override fun isSubtypeOf(other: EnhancedTypeForWarningAttribute?): Boolean = true
+    override fun toString(): String = "Enhanced for warning(${coneType.renderForDebugging()})"
+    override fun copyWith(newType: ConeKotlinType): EnhancedTypeForWarningAttribute = EnhancedTypeForWarningAttribute(newType)
+
+    override val key: KClass<out EnhancedTypeForWarningAttribute>
+        get() = EnhancedTypeForWarningAttribute::class
+}
+
+val ConeAttributes.enhancedTypeForWarning: EnhancedTypeForWarningAttribute? by ConeAttributes.attributeAccessor<EnhancedTypeForWarningAttribute>()
+
+val ConeKotlinType.enhancedTypeForWarning: ConeKotlinType?
+    get() = attributes.enhancedTypeForWarning?.coneType
