5ac46d7684cca57d4bfd6116697239c3550f33e4
Alexander Shabalin <alexander.shabalin@jetbrains.com>
2023-10-23 16:11:12 +0200

    [K/N] Migrate runtime/text tests to new testing infra ^KT-61259

--- kotlin-native/runtime/test/text/CharNativeTest.kt
+++ kotlin-native/runtime/test/text/CharNativeTest.kt
@@ -63,10 +63,95 @@ class CharNativeTest {
         assertEquals("\u0069\u0307", '\u0130'.lowercase())
     }
 
+    @Test
     fun titlecase() {
         // titlecase = titlecaseChar = char != uppercaseChar
         assertEquals('\u10F0'.titlecaseChar().toString(), '\u10F0'.titlecase())
         assertEquals('\u10F0', '\u10F0'.titlecaseChar())
         assertNotEquals('\u10F0', '\u10F0'.uppercaseChar())
+    }
+
+    @Test
+    fun testIsSupplementaryCodePoint() {
+        assertFalse(Char.isSupplementaryCodePoint(-1))
+        for (c in 0..0xFFFF) {
+            assertFalse(Char.isSupplementaryCodePoint(c.toInt()))
+        }
+        for (c in 0xFFFF + 1..0x10FFFF) {
+            assertTrue(Char.isSupplementaryCodePoint(c))
+        }
+        assertFalse(Char.isSupplementaryCodePoint(0x10FFFF + 1))
+    }
+
+    @Test
+    fun testIsSurrogatePair() {
+        assertFalse(Char.isSurrogatePair('\u0000', '\u0000'))
+        assertFalse(Char.isSurrogatePair('\u0000', '\uDC00'))
+        assertTrue( Char.isSurrogatePair('\uD800', '\uDC00'))
+        assertTrue( Char.isSurrogatePair('\uD800', '\uDFFF'))
+        assertTrue( Char.isSurrogatePair('\uDBFF', '\uDFFF'))
+        assertFalse(Char.isSurrogatePair('\uDBFF', '\uF000'))
+    }
+
+    @Test
+    fun testToChars() {
+        assertTrue(charArrayOf('\uD800', '\uDC00').contentEquals(Char.toChars(0x010000)))
+        assertTrue(charArrayOf('\uD800', '\uDC01').contentEquals(Char.toChars(0x010001)))
+        assertTrue(charArrayOf('\uD801', '\uDC01').contentEquals(Char.toChars(0x010401)))
+        assertTrue(charArrayOf('\uDBFF', '\uDFFF').contentEquals(Char.toChars(0x10FFFF)))
+
+        try {
+            Char.toChars(Int.MAX_VALUE)
+            throw AssertionError()
+        } catch (e: IllegalArgumentException) {}
+    }
+
+    @Test
+    fun testToCodePoint() {
+        assertEquals(0x010000, Char.toCodePoint('\uD800', '\uDC00'))
+        assertEquals(0x010001, Char.toCodePoint('\uD800', '\uDC01'))
+        assertEquals(0x010401, Char.toCodePoint('\uD801', '\uDC01'))
+        assertEquals(0x10FFFF, Char.toCodePoint('\uDBFF', '\uDFFF'))
+    }
+
+    @Test
+    fun testCase() {
+        assertEquals('A', 'a'.uppercaseChar())
+        assertEquals('A', 'A'.uppercaseChar())
+        assertEquals('1', '1'.uppercaseChar())
+
+        assertEquals('a', 'A'.lowercaseChar())
+        assertEquals('a', 'a'.lowercaseChar())
+        assertEquals('1', '1'.lowercaseChar())
+    }
+
+    @Test
+    fun testCategory() {
+        assertTrue('\n'     in CharCategory.CONTROL)
+        assertTrue('1'      in CharCategory.DECIMAL_DIGIT_NUMBER)
+        assertTrue(' '      in CharCategory.SPACE_SEPARATOR)
+        assertTrue('a'      in CharCategory.LOWERCASE_LETTER)
+        assertTrue('A'      in CharCategory.UPPERCASE_LETTER)
+        assertTrue('<'      in CharCategory.MATH_SYMBOL)
+        assertTrue(';'      in CharCategory.OTHER_PUNCTUATION)
+        assertTrue('_'      in CharCategory.CONNECTOR_PUNCTUATION)
+        assertTrue('$'      in CharCategory.CURRENCY_SYMBOL)
+        assertTrue('\u2029' in CharCategory.PARAGRAPH_SEPARATOR)
+    }
+
+    @Test
+    fun testIsHighSurrogate() {
+        assertTrue('\uD800'.isHighSurrogate())
+        assertTrue('\uDBFF'.isHighSurrogate())
+        assertFalse('\uDC00'.isHighSurrogate())
+        assertFalse('\uDFFF'.isHighSurrogate())
+    }
+
+    @Test
+    fun testIsLowSurrogate() {
+        assertFalse('\uD800'.isLowSurrogate())
+        assertFalse('\uDBFF'.isLowSurrogate())
+        assertTrue('\uDC00'.isLowSurrogate())
+        assertTrue('\uDFFF'.isLowSurrogate())
     }
 }
--- kotlin-native/runtime/test/text/StringNativeTest.kt
+++ kotlin-native/runtime/test/text/StringNativeTest.kt
@@ -103,4 +103,99 @@ class StringNativeTest {
         // Non-ASCII
         assertEquals("\u00DE\u03A9\u0403\uA779", "\u00FE\u03A9\u0453\uA77A".uppercase())
     }
+
+    @Test
+    fun indexOf() {
+        var str = "Hello World!!"    // for indexOf String
+        var ch = 'a'                 // for indexOf Char
+
+        assertEquals(6, str.indexOf("World", 0))
+        assertEquals(6, str.indexOf("World", -1))
+
+        assertEquals(-1, str.indexOf(ch, 0))
+
+        str = "Kotlin/Native"
+        assertEquals(-1, str.indexOf("/", str.length + 1))
+        assertEquals(-1, str.indexOf("/", Int.MAX_VALUE))
+        assertEquals(str.length, str.indexOf("", Int.MAX_VALUE))
+        assertEquals(1, str.indexOf("", 1))
+
+        assertEquals(8, str.indexOf(ch, 1))
+        assertEquals(-1, str.indexOf(ch, str.length - 1))
+
+        str = ""
+        assertEquals(-1, str.indexOf("a", -3))
+        assertEquals(0, str.indexOf("", 0))
+
+        assertEquals(-1, str.indexOf(ch, -3))
+        assertEquals(-1, str.indexOf(ch, 10))
+
+        ch = 0.toChar()
+        assertEquals(-1, str.indexOf(ch, -3))
+        assertEquals(-1, str.indexOf(ch, 10))
+    }
+
+    @Test
+    fun runTest() {
+        val str = "hello"
+        assertTrue(str.equals("HElLo", true))
+        val strI18n = "Привет"
+        assertTrue(strI18n.equals("прИВет", true))
+        assertEquals("ПРИВЕТ", strI18n.toUpperCase())
+        assertEquals("привет", strI18n.toLowerCase())
+        assertEquals("Пока", "пока".capitalize())
+        assertTrue("http://jetbrains.com".startsWith("http://"))
+    }
+
+    @Test
+    fun trimWhitespaces() {
+        assertEquals(expected = "String", actual = "  String".trim(), message = "Trim leading spaces")
+        assertEquals(expected = "String  ", actual = "    String  ".trimStart(), message = "Trim start")
+        assertEquals(expected = "  String", actual = "  String \t ".trimEnd(), message = "Trim end")
+
+        assertEquals(expected = "String", actual = "\u0020 \u202FString\u2028\u2029".trim(),
+                message = "Trim special whitespaces")
+        assertEquals(expected = "\u1FFFString", actual = "\u00A0  \u1FFFString".trim(),
+                message = "Trim special whitespace but should left a unicode symbol")
+        assertEquals(expected = "String\tSTR", actual = " \nString\tSTR  ".trim(), message = "Trim newline")
+    }
+
+    @Test
+    fun testIntToStringWithRadix() {
+        assertEquals(2147483647.toString(8),  "17777777777", "Octal string")
+        assertEquals(2147483647.toString(16), "7fffffff", "Hex string")
+        assertEquals(2147483647.toString(2),  "1111111111111111111111111111111", "Binary string")
+        assertEquals(2147483647.toString(10), "2147483647", "Decimal string")
+
+        assertEquals((-2147483647).toString(8),  "-17777777777", "Octal string")
+        assertEquals((-2147483647).toString(16), "-7fffffff", "Hex string")
+        assertEquals((-2147483647).toString(2),  "-1111111111111111111111111111111", "Binary string")
+        assertEquals((-2147483647).toString(10), "-2147483647", "Decimal string")
+
+        assertEquals((-2147483648).toString(8),  "-20000000000", "Octal string")
+        assertEquals((-2147483648).toString(16), "-80000000", "Hex string")
+        assertEquals((-2147483648).toString(2),  "-10000000000000000000000000000000", "Binary string")
+        assertEquals((-2147483648).toString(10), "-2147483648", "Decimal string")
+    }
+
+    @Test
+    fun testLongToStringWithRadix() {
+        assertEquals(100000000L.toString(10), "100000000", "Decimal string")
+        assertEquals(68719476735L.toString(16), "fffffffff", "Hex string")
+        assertEquals(8589934591L.toString(8), "77777777777", "Octal string")
+        assertEquals(8796093022207L.toString(2), "1111111111111111111111111111111111111111111", "Binary string")
+
+        assertEquals((-0x7fffffffffffffffL - 1).toString(10), "-9223372036854775808", "Min decimal string")
+        assertEquals(0x7fffffffffffffffL.toString(10), "9223372036854775807", "Max decimal string")
+        assertEquals((-0x7fffffffffffffffL - 1).toString(16), "-8000000000000000", "Min hex string")
+        assertEquals(0x7fffffffffffffffL.toString(16), "7fffffffffffffff", "Max hex string")
+    }
+
+    @Test
+    fun testCase() {
+        assertEquals("\ud801\udc00", "\ud801\udc28".uppercase())
+        assertEquals("\ud801\udc00", "\ud801\udc00".uppercase())
+        assertEquals("\ud801\udc28", "\ud801\udc28".lowercase())
+        assertEquals("\ud801\udc28", "\ud801\udc00".lowercase())
+    }
 }
