824e81f2c9f4a1e15952b6b3ac7d744172d1c73f
Brian Norman <brian.norman@jetbrains.com>
2023-10-26 11:32:26 -0500

    [FIR] Do not include transitive friend dependencies in symbol provider
    When flattening a dependency FirSymbolProvider, make sure transitive
    dependency FirSymbolProviders are not included. This requires checking
    that nested symbol provider sessions match the composite symbol provider
    session when they are both source sessions.
    
    ^KT-60614 Fixed

--- compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirLightTreeOldFrontendDiagnosticsTestGenerated.java
+++ compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirLightTreeOldFrontendDiagnosticsTestGenerated.java
@@ -22923,6 +22923,12 @@ public class FirLightTreeOldFrontendDiagnosticsTestGenerated extends AbstractFir
             }
 
             @Test
+            @TestMetadata("dependencyModule.kt")
+            public void testDependencyModule() throws Exception {
+                runTest("compiler/testData/diagnostics/tests/multimodule/dependencyModule.kt");
+            }
+
+            @Test
             @TestMetadata("dependsOnModule.kt")
             public void testDependsOnModule() throws Exception {
                 runTest("compiler/testData/diagnostics/tests/multimodule/dependsOnModule.kt");
