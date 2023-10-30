68d1276329d402862cb3450d853dca057ed3ebe3
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2023-10-30 11:23:36 +0100

    K2: add test for KT-56520, case (4), callable references (bound)
    In this test, things work in the similar way as in constructors case (2).
    K1 resolves the explicit receiver HashMap<String, String>()
    to kotlin.collections.HashMap /* = java.util.HashMap */.
    K2 does the similar, but fun processConstructors from ConstructorProcessing.kt
    makes a type alias substitution, so in fact constructor of expanded
    java.util.HashMap is processed.
    
    Pay attention that both frontends ignore some.HashMap and java.util.HashMap
    due to ambiguous classifiers in explicit star importing scope.
    See FirScope.processConstructorsByName in ConstructorProcessing.kt

--- analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/LLFirPreresolvedReversedDiagnosticCompilerFE10TestDataTestGenerated.java
+++ analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/LLFirPreresolvedReversedDiagnosticCompilerFE10TestDataTestGenerated.java
@@ -42064,6 +42064,12 @@ public class LLFirPreresolvedReversedDiagnosticCompilerFE10TestDataTestGenerated
                 }
 
                 @Test
+                @TestMetadata("callableReferenceCase.kt")
+                public void testCallableReferenceCase() throws Exception {
+                    runTest("compiler/testData/diagnostics/testsWithStdLib/resolve/ambiguousTypeReferences/callableReferenceCase.kt");
+                }
+
+                @Test
                 @TestMetadata("classLiteralCase.kt")
                 public void testClassLiteralCase() throws Exception {
                     runTest("compiler/testData/diagnostics/testsWithStdLib/resolve/ambiguousTypeReferences/classLiteralCase.kt");
--- compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirLightTreeOldFrontendDiagnosticsTestGenerated.java
+++ compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirLightTreeOldFrontendDiagnosticsTestGenerated.java
@@ -42008,6 +42008,12 @@ public class FirLightTreeOldFrontendDiagnosticsTestGenerated extends AbstractFir
                 }
 
                 @Test
+                @TestMetadata("callableReferenceCase.kt")
+                public void testCallableReferenceCase() throws Exception {
+                    runTest("compiler/testData/diagnostics/testsWithStdLib/resolve/ambiguousTypeReferences/callableReferenceCase.kt");
+                }
+
+                @Test
                 @TestMetadata("classLiteralCase.kt")
                 public void testClassLiteralCase() throws Exception {
                     runTest("compiler/testData/diagnostics/testsWithStdLib/resolve/ambiguousTypeReferences/classLiteralCase.kt");
