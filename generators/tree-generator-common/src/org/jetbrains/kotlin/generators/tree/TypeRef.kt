d64dda0fa0b45c757bc30cc3d1feef6dd2a5a2a0
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2023-10-30 10:52:48 +0100

    [IR/FIR generator] Fix TypeVariable.copy method

--- generators/tree-generator-common/src/org/jetbrains/kotlin/generators/tree/TypeRef.kt
+++ generators/tree-generator-common/src/org/jetbrains/kotlin/generators/tree/TypeRef.kt
@@ -207,7 +207,7 @@ open class NamedTypeParameterRef(
         renderNullabilityTo(appendable)
     }
 
-    final override fun copy(nullable: Boolean) = NamedTypeParameterRef(name, nullable)
+    override fun copy(nullable: Boolean) = NamedTypeParameterRef(name, nullable)
 }
 
 interface TypeRefWithNullability : TypeRef {
@@ -256,7 +256,10 @@ class TypeVariable(
     name: String,
     val bounds: List<TypeRef>,
     val variance: Variance,
-) : NamedTypeParameterRef(name)
+    nullable: Boolean = false,
+) : NamedTypeParameterRef(name, nullable) {
+    final override fun copy(nullable: Boolean) = TypeVariable(name, bounds, variance, nullable)
+}
 
 fun <P : TypeParameterRef> KClass<*>.asRef(): ClassRef<P> {
     val qualifiedName = this.qualifiedName ?: error("$this doesn't have qualified name and thus cannot be converted to ClassRef")
