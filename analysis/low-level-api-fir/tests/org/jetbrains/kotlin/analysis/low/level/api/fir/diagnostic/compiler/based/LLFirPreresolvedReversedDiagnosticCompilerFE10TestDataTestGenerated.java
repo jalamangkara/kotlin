367fe1d495d3c8a0a8cddc9b0577e5c4684ddbca
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2023-10-30 11:41:15 +0100

    K2: add test for KT-56520, case (4), callable references (unbound)
    Again, both frontends here ignored classifiers from
    explicit star imported scope (some.HashMap, java.util.HashMap)
    because of their ambiguity. In case of K2, it works so due to logic
    in BodyResolveComponents.resolveRootPartOfQualifier.
    This function is called to resolve qualifier without receiver.
    See also cases (3) and (7).

--- analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/LLFirPreresolvedReversedDiagnosticCompilerFE10TestDataTestGenerated.java
+++ analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/LLFirPreresolvedReversedDiagnosticCompilerFE10TestDataTestGenerated.java
@@ -42070,6 +42070,12 @@ public class LLFirPreresolvedReversedDiagnosticCompilerFE10TestDataTestGenerated
                 }
 
                 @Test
+                @TestMetadata("callableReferenceUnboundCase.kt")
+                public void testCallableReferenceUnboundCase() throws Exception {
+                    runTest("compiler/testData/diagnostics/testsWithStdLib/resolve/ambiguousTypeReferences/callableReferenceUnboundCase.kt");
+                }
+
+                @Test
                 @TestMetadata("classLiteralCase.kt")
                 public void testClassLiteralCase() throws Exception {
                     runTest("compiler/testData/diagnostics/testsWithStdLib/resolve/ambiguousTypeReferences/classLiteralCase.kt");
